53760879dbda91343b63aa4b059c611b6812788b
anamariabrb <anamaria.barbatescu@outlook.com>
2023-05-26 17:53:53 +0300

    curs6 final

---
+++ src/curs6/ForEachExample.java
@@ -0,0 +1,17 @@
+package curs6;
+
+public class ForEachExample {
+
+	public static void main(String[] args) {
+		String [] orase = {"Iasi", "Cluj", "Brasov", "Pitesti", "Ploiesti"};
+
+	for (int i=0; i<orase.length; i++) {
+		
+		System.out.println(orase[i]);
+		
+		
+	}
+	System.out.println ("---------");
+	}
+
+}
