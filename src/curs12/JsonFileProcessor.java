e1bc1c7daebf7c3fdb9ca4f24e28690054cae754
anamariabrb <132680450+anamariabrb@users.noreply.github.com>
2023-06-23 17:56:54 +0300

    curs13 final

---
+++ src/curs12/JsonFileProcessor.java
@@ -0,0 +1,14 @@
+package curs12;
+
+import org.json.simple.JSONObject;
+public class JsonFileProcessor {
+
+	public static void main(String[] args) {
+		
+		JSONObject obj = new JSONObject();
+	}
+
+	
+	
+
+}
---
+++ src/curs12/TestProcessor.java
@@ -0,0 +1,12 @@
+package curs12;
+
+public class TestProcessor {
+
+	public static void main(String[] args) {
+		
+		JsonFileProcessor obj = new JsonFileProcessor();
+		obj.writeJson();
+
+	}
+
+}
